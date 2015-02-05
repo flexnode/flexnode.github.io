f52677763d6540ca4fd994ec6d243fd6b523799b
TS Lim <tslim@flexnode.com>
2015-02-05 23:11:16 +0800

    Track section on navbar

--- javascripts/application.js
+++ javascripts/application.js
@@ -11,9 +11,32 @@ $(document).ready(function() {
     }
   });
 
+  // Show which section is looking at on navbar
+  $(window).scroll(function (event) {
+    var scroll_position = $(window).scrollTop();
+
+    $(".navTop a").each(function(index) {
+      var anchor = $($(this).attr("href"));
+      var anchor_top = anchor.offset().top - 50;
+      var anchor_bottom = anchor_top + anchor.height() -1;
+
+      console.log(anchor_top);
+      console.log(anchor_bottom);
+
+      if (scroll_position >= anchor_top && scroll_position <= anchor_bottom) {
+        $(this).addClass("is-active");
+      } else {
+        $(this).removeClass("is-active");
+      }
+
+    });
+
+  });
+
+  // Scroll to section
   $(".js-scroll-to").click(function (event) {
     event.preventDefault();
-    var anchor = $("" + $(event.currentTarget).attr("href"));
+    var anchor = $($(event.currentTarget).attr("href"));
     $("html, body").animate({ scrollTop: anchor.offset().top - 50 });
     $(window).scroll();
   });
