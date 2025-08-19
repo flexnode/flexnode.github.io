95481baab94b69b79e3b13d6475e2f22a93f68f2
tslim <tslim@flexnode.com>
2025-08-19 17:03:03 +0800

    Change logo

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -68,16 +68,14 @@ function Header({
           onMouseEnter={() => setLogoHovered(true)}
           onMouseLeave={() => setLogoHovered(false)}
         >
-          <Logomark
-            className="h-8 sm:hidden"
-            invert={invert}
-            filled={logoHovered}
-          />
-          <Logo
-            className="hidden h-8 sm:block"
-            invert={invert}
-            filled={logoHovered}
-          />
+          <span
+            className={clsx(
+              'font-display text-3xl font-medium tracking-tight text-balance',
+              invert ? 'text-white' : 'text-sky-700',
+            )}
+          >
+            Flexnode
+          </span>
         </Link>
         <div className="flex items-center gap-x-8">
           <Button href="#contact-us" invert={invert}>
