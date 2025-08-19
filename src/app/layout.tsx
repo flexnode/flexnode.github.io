67ec0de544c31dc58421272d1ea09eda4e58e8e1
tslim <tslim@flexnode.com>
2025-08-19 17:26:01 +0800

    Add GA code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import Script from 'next/script'
 
 import '@/styles/tailwind.css'
 
@@ -10,8 +11,28 @@ export const metadata: Metadata = {
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const gaId = 'G-FN7FNMGXNM'
+
   return (
     <html lang="en" className="h-full bg-sky-900 text-base antialiased">
+      <head>
+        {gaId && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaId}');
+              `}
+            </Script>
+          </>
+        )}
+      </head>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   )
